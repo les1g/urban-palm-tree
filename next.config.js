176462ed64938156d679769eb4528ff856b03778
Gisel Garrido <ggarr55@wgu.edu>
2025-12-16 18:22:46 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['your-image-host.com'],
   },
 };
 
 module.exports = nextConfig;
+
